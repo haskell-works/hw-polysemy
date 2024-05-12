12d7087813f1252f65467c5900e677bb9f841028
John Ky <john.ky@iohk.io>
2024-05-12 23:06:40 +1000

    New onLeft and onNothing functions

--- src/HaskellWorks/Polysemy/Error.hs
+++ src/HaskellWorks/Polysemy/Error.hs
@@ -1,12 +1,20 @@
 module HaskellWorks.Polysemy.Error
-  ( onLeftM
+  ( onLeft
+  , onNothing
+  , onLeftM
   , onNothingM
   ) where
 
 import           HaskellWorks.Polysemy.Prelude
 
+onLeft :: Monad m => (e -> m a) -> Either e a -> m a
+onLeft f = either f pure
+
+onNothing :: Monad m => m b -> Maybe b -> m b
+onNothing h = maybe h return
+
 onLeftM :: Monad m => (e -> m a) -> m (Either e a) -> m a
-onLeftM f action = either f pure =<< action
+onLeftM f action = onLeft f =<< action
 
 onNothingM :: Monad m => m b -> m (Maybe b) -> m b
-onNothingM h f = f >>= maybe h return
+onNothingM h f = onNothing h =<< f
