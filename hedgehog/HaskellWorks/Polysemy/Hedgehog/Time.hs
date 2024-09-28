ed54f32dbdb58f124691c1f0cd4c59d8de5964f4
John Ky <john.ky@iohk.io>
2024-09-28 13:55:23 +1000

    New ulid and time generator functions

---
+++ hedgehog/HaskellWorks/Polysemy/Hedgehog/Time.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE TypeApplications   #-}
+
+module HaskellWorks.Polysemy.Hedgehog.Time (
+    genPOSIXTime,
+) where
+
+import           Data.Time.Clock.POSIX (POSIXTime)
+
+import           Hedgehog
+import qualified Hedgehog.Gen          as Gen
+import qualified Hedgehog.Range        as Range
+
+import           HaskellWorks.Prelude
+
+genPOSIXTime :: Gen POSIXTime
+genPOSIXTime = do
+    -- Generate a random integer within a reasonable range for POSIX time
+    -- POSIXTime is a type synonym for NominalDiffTime, which is in seconds
+    -- We'll use a range from 0 to a large number of seconds to cover a wide time span
+    seconds <- Gen.integral (Range.linear 0 4_102_444_800) -- Up to year 2100
+    return $ fromIntegral @Word64 seconds
