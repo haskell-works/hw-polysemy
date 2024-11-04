b27a81e54f8b5188adcce37c75283872464bdc2f
John Ky <john.ky@iohk.io>
2024-11-05 00:02:30 +1100

    Re-export Gen and Range modules

---
+++ hedgehog/HaskellWorks/Polysemy/Hedgehog/Gen.hs
@@ -0,0 +1,5 @@
+module HaskellWorks.Polysemy.Hedgehog.Gen
+  ( module G
+  ) where
+
+import           Hedgehog.Gen as G
---
+++ hedgehog/HaskellWorks/Polysemy/Hedgehog/Range.hs
@@ -0,0 +1,5 @@
+module HaskellWorks.Polysemy.Hedgehog.Range
+  ( module R
+  ) where
+
+import           Hedgehog.Range as R
