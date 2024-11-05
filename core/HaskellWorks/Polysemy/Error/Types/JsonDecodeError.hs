adbd4fa1d93c3f60cef2b77b6569b6974a3065c7
John Ky <john.ky@iohk.io>
2024-11-05 14:10:54 +1100

    Add ToJSON instances for JsonDecodeError and YamlDecodeError

--- core/HaskellWorks/Polysemy/Error/Types/JsonDecodeError.hs
+++ core/HaskellWorks/Polysemy/Error/Types/JsonDecodeError.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 
@@ -5,10 +6,24 @@ module HaskellWorks.Polysemy.Error.Types.JsonDecodeError
   ( JsonDecodeError(..)
   ) where
 
+
+import           Data.Aeson                (ToJSON (..), (.=))
+import qualified Data.Aeson                as J
+import           GHC.Generics
+
+import           Data.Generics.Product.Any
 import           HaskellWorks.Prelude
+import           Lens.Micro
 
 newtype JsonDecodeError =
   JsonDecodeError
   { message :: String
   }
   deriving (Eq, Generic, Show)
+
+instance ToJSON JsonDecodeError where
+  toJSON e =
+        J.object
+            [ "error" .= id @Text "JsonDecodeError"
+            , "message" .= (e ^. the @"message")
+            ]
